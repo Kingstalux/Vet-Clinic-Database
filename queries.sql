5cef458b42cd9b7fb971ba50fc47c89d468c120a
Kingstalux <kingsleyngu25@gmail.com>
2021-12-20 14:40:42 +0100

    Add queries

--- queries.sql
+++ queries.sql
@@ -54,3 +54,23 @@ UPDATE animals
 SET weight_kg = weight_kg * -1
 WHERE weight_kg < 0;
 COMMIT;
+
+SELECT COUNT(name) FROM animals;
+
+SELECT COUNT(name) FROM animals
+WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, SUM(escape_attempts) FROM animals
+GROUP BY neutered;
+
+SELECT neutered, MIN(weight_kg) FROM animals
+GROUP BY neutered;
+
+SELECT neutered, MAX(weight_kg) FROM animals
+GROUP BY neutered;
+
+SELECT neutered, AVG(escape_attempts) FROM animals
+WHERE date_of_birth >= '1990-01-01' and date_of_birth <= '2000-01-01'
+GROUP BY neutered;
