7c1cde3011b4c37f05ee1e0071ed908b8f72d69f
Kingstalux <kingsleyngu25@gmail.com>
2021-12-21 07:13:38 +0100

    Add foreign keys

--- schema.sql
+++ schema.sql
@@ -11,3 +11,32 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD species text;
+
+CREATE TABLE owners(
+	id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	full_name text,
+	age integer
+);
+
+CREATE TABLE species(
+	id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name text
+);
+
+ALTER TABLE animals
+DROP COLUMN id;
+
+ALTER TABLE animals
+ADD id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY;
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD species_id integer,
+ADD FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals
+ADD owners_id integer,
+ADD FOREIGN KEY (owners_id) REFERENCES owners(id);
+
