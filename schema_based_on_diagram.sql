dad86122e6be7086ee6febed1c33eb8be7da5a21
alexander16108 <alexandermayowa05@gmail.com>
2022-01-04 14:49:47 +0100

    adding constraint and m-1-m relations between tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,35 +1,53 @@
 CREATE TABLE patients(
-    ID SERIAL GENERATED AS IDENTITY PRIMARY KEY,
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR,
     date_of_birth date
 );
 
 CREATE TABLE treatments(
-    ID SERIAL GENERATED AS IDENTITY PRIMARY KEY,
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     type VARCHAR,
     name VARCHAR
 );
 
 CREATE TABLE invoices(
-    ID SERIAL GENERATED AS IDENTITY PRIMARY KEY,
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     total_amount decimal,
     generated_at timestamp,
     payed_at timestamp,
-    medical_history_id INT
+    medical_history_id INT,
+     CONSTRAINT fk_medical_his FOREIGN KEY(medical_history_id)
+    REFERENCES medical_histories(id)
 );
 
 CREATE TABLE medical_histories(
-    ID SERIAL GENERATED AS IDENTITY PRIMARY KEY,
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     admitted_at timestamp,
     patient_id INT,
-    status VARCHAR
+    status VARCHAR,
+     CONSTRAINT fk_patients FOREIGN KEY(patient_id)
+    REFERENCES patients(id)
 );
 
 CREATE TABLE invoice_items(
-    ID SERIAL GENERATED AS IDENTITY PRIMARY KEY,
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     unit_price decimal,
     quantity INT,
     total_price decimal,
     invoice_id INT,
-    treatment_id INT
+    treatment_id INT,
+     CONSTRAINT fk_invoice FOREIGN KEY(invoice_id)
+    REFERENCES invoices(id),
+  CONSTRAINT fk_treatment FOREIGN KEY(treatment_id)
+    REFERENCES treatments(id)
+);
+
+-- creating a many to many relationship between treatment and medical_history
+CREATE TABLE medical_treatment(
+    medical_id INT,
+    treatment_id INT,
+    CONSTRAINT fk_medical_history FOREIGN KEY(medical_id)
+    REFERENCES medical_histories(id),
+    CONSTRAINT fk_medical_treatment FOREIGN KEY(treatment_id)
+    REFERENCES treatments(id)
 );
